5db71c971f2ab15a94cbe9fcc34db89c89f7c3a5
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2024-05-05 01:15:10 -0300

    add: Add border-radius ao .container

--- main.js
+++ main.js
@@ -15,15 +15,14 @@ function verificarCadastro() {
     if (nome === '' || telefone === '') {
         alert('Por favor preenche todos os campos')
     } else {
-        // Verificar se o nome já está na tabela
-    let tabela = document.getElementById("tabelaContatos");
+        
+    let tabela = document.getElementById("tabela-contatos");
     let linhas = tabela.rows;
 
     for (let i = 1; i < linhas.length; i++) {
             let celulaNome = linhas[i].cells[0];
             let nomeExistente = celulaNome.textContent.trim();
 
-            // Usar expressão regular para fazer a comparação
             let regex = new RegExp("^" + nome + "$", "i");
             if (regex.test(nomeExistente)) {
                 alert("Este nome já está cadastrado na agenda.");
@@ -35,18 +34,15 @@ function verificarCadastro() {
 }
 
 function adicionarContato(nome, telefone) {
-    let tabela = document.getElementById("tabelaContatos");
+    let tabela = document.getElementById("tabela-contatos");
     let novaLinha = tabela.insertRow(-1);
-    
-    // Insere células para nome e telefone
+
     let celulaNome = novaLinha.insertCell(0);
     let celulaTelefone = novaLinha.insertCell(1); 
 
-    // Define o conteúdo das células
     celulaNome.textContent = nome;
     celulaTelefone.textContent = telefone;
 
-    // Limpar os campos do formulário após adicionar o contato
     document.getElementById("nome").value = "";
     document.getElementById("telefone").value = "";
 }
